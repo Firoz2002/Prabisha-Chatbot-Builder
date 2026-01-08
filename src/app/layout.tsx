5a89f614cc1fd97a23f336f8b3c9813e20d3a00f
FirozKamdar <firozkamdar01@gmail.com>
2026-01-08 16:48:22 +0530

    chatbot added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -49,7 +49,7 @@ export default function RootLayout({
                     w[o]=w[o]||function(){(w[o].q=w[o].q||[]).push(arguments)};
                     js=d.createElement(s),fjs=d.getElementsByTagName(s)[0];
                     js.id=o;js.src=f;js.async=1;fjs.parentNode.insertBefore(js,fjs);
-                  }(window,document,'script','chatbot','http://localhost:3000/embed.js'));
+                  }(window,document,'script','chatbot','https://chatbots.prabisha.com/embed.js'));
 
                   chatbot('init', {
                     chatbotId: 'cmk57u7c8000004jufgkkdhj0',
